a912e9b1b3ae11db107fe56a15fc82be678b9c2b
Javedgouri <javeda3740@gmail.com>
2016-07-08 09:19:32 +0530

    commit on 8 july

--- application/controllers/Campaign.php
+++ application/controllers/Campaign.php
@@ -13,7 +13,7 @@ Class Campaign extends CI_Controller{
 		$SPVTenancyInformationres=$this->Getinfo->getSPVTenancyInformation($PropertyCampaignId);
 		$this->load->view('header');
 		$this->load->view('campaignpage',$result,$imageresult,$Appreciationres,$SPVTenancyInformationres);
-		$this->load->view();
+		$this->load->view('footer');
 		print_r($result);
 		print_r($imageresult);
 		print_r($Appreciationres);
